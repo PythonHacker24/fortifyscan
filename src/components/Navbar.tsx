b49808d41929ae4d49a9bc5423a752306882521c
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-14 00:35:29 +0530

    Removed GITHUB Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,6 @@ export default function Navbar() {
         <h1 className="text-2xl font-bold text-gray-100">Raincheck</h1>
 
         <div className="flex items-center gap-4">
-          <Link
-            href="https://github.com/PythonHacker24/fortifyscan"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center gap-2 px-4 py-2 rounded-lg bg-gray-900 hover:bg-gray-800 text-gray-100 transition-colors"
-          >
-            {/* <CircleUserIcon className="w-5 h-5" />
-            <span>Log In</span> */}
-
-            <GithubIcon className='w-5 h-5' />
-              <span>GitHub</span>
-          </Link>
           
           <Link
             href="https://getwaitlist.com/waitlist/29359"
